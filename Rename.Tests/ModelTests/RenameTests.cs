0eb9438223e5452c861e781abf2ed477bc2fad97
Adam Titus and James Osborn <adam+james@>
2018-01-09 09:47:08 -0800

    Add testing.

---
+++ Rename.Tests/ModelTests/RenameTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rename.Models;
+
+namespace Rename.Tests
+{
+    [TestClass]
+    public class RenameTests
+    {
+        [TestMethod]
+        public void Test1_Describe_Result()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
